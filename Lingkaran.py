3c4796299a3bcfcebcc1e44c7fed96d4b9d616c4
ReOrize5 <115756982+ReOrize5@users.noreply.github.com>
2022-10-30 12:17:18 +0700

    Create Lingkaran.py

---
+++ Lingkaran.py
@@ -0,0 +1,7 @@
+pi = 3.14
+r = float(input("Masukkan Jari-Jari: "))
+luas = pi* r ** 2
+keliling = 2 * pi * r
+
+print("Luas Lingkaran =","{:.1f}".format(luas))
+print("Keliling Lingkaran =","{:.1f}".format(keliling))
